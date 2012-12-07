1e4c32da2890e0ae93f2723b5a6b7492af885551
Steve Tibbetts <steve@scrubbingmachine.com>
2012-12-06 23:57:59 -0800

    Added Extra Green Sorting, code cleanup

--- SmartBag.lua
+++ SmartBag.lua
@@ -3,7 +3,7 @@
 -- *********************************************
 function SmartBag_OnLoad()
 	-- The I'm alive message
-	print("<Smart Bag v0.7>")
+	print("<Smart Bag v0.8>")
 
 	-- Register slash commands
 	SlashCmdList["SMARTBAG"] = SmartBag_SlashCommand
@@ -14,6 +14,7 @@ function SmartBag_OnLoad()
   if (not SmartBagSettings) then
   SmartBagSettings = {}
   SmartBagSettings["AutoSellGrey"]=false
+  SmartBagSettings["SortGrey"]="0"
   SmartBagSettings["EnchangingBag"]="0"
   SmartBagSettings["MiningBag"]="0"
   SmartBagSettings["InscriptionBag"]="0"
@@ -21,23 +22,24 @@ function SmartBag_OnLoad()
   SmartBagSettings["GearSetBag"]="0"
   SmartBagSettings["FirstRun"] = "0"
   SmartBagSettings["Alerts"]=true
+  SmartBagSettings["BlueSort"]="0"
+  SmartBagSettings["GreenSort"]="0"
+
   end
-
-
-
 end
 
 function ExecuteSorting()
-
-  SortNonEquipmentSetItems(20,2)
   SortEquipmentSet(SmartBagSettings["GearSetBag"])
-
+  SortRarity(2,SmartBagSettings["GreenSort"])
 end
 
--- Handle events
+-- Event Handlers
 function SmartBag_EventHandler(self, event, ...)
  if event == "MERCHANT_SHOW" then 
-  if  SmartBagSettings["AutoSellGrey"] == true then SellGrey() end
+  if  SmartBagSettings["AutoSellGrey"] == true then 
+    SellGrey()
+  end
+  ExecuteSorting()
  end
 
  if event == "PLAYER_LOGOUT" then 
@@ -45,7 +47,6 @@ function SmartBag_EventHandler(self, event, ...)
  end
 
  if event == "PLAYER_LEAVE_COMBAT" then
-  print("MONKEY!!")
   ExecuteSorting()
  end
 
@@ -59,23 +60,49 @@ function SmartBag_EventHandler(self, event, ...)
     SetButttonText(SellGreyButton,SmartBagSettings["AutoSellGrey"])
     SetButttonText(KeepEquipmentButton,SmartBagSettings["GearSetBag"])
     SetButttonText(AlertTextButton,SmartBagSettings["Alerts"])
+    SetButttonText(GreenSortButton,SmartBagSettings["GreenSort"])
 
  end
 
 end
 
--- Slash Command Handling
+-- Slash Commands
 function SmartBag_SlashCommand(msg)
   if SmartBagSettingsWindow:IsVisible() then SmartBagSettingsWindow:Hide() else SmartBagSettingsWindow:Show() end
 end
 
+-- *********************************************
+-- UI Functions
+-- *********************************************
+
+function SellGreyButton_OnClick()
+   SmartBagSettings["AutoSellGrey"] = UpdateSettingChoice(SmartBagSettings["AutoSellGrey"])
+   SetButttonText(SellGreyButton,SmartBagSettings["AutoSellGrey"])
+end
+
+function KeepEquipmentButton_OnClick()
+   SmartBagSettings["GearSetBag"] = UpdateSettingChoice(SmartBagSettings["GearSetBag"])
+   SetButttonText(KeepEquipmentButton,SmartBagSettings["GearSetBag"])
+end
+
+function GreenSortButton_OnClick()
+   SmartBagSettings["GreenSort"] = UpdateSettingChoice(SmartBagSettings["GreenSort"])
+   SetButttonText(GreenSortButton,SmartBagSettings["GreenSort"])
+end
+
+function AlertTextButton_OnClick()
+   SmartBagSettings["Alerts"] = UpdateSettingChoice(SmartBagSettings["Alerts"])
+   SetButttonText(AlertTextButton,SmartBagSettings["Alerts"])
+end
+
+function OkButton_OnClick()
+  SmartBagSettingsWindow:Hide()
+end
 
 -- *********************************************
--- Utility Functions
+-- Sorting Functions
 -- *********************************************
 
--- Move a single item by name to the "targetbag"
--- NEED TO LOOK AT THIS: CLEANUP / SPACE CHECKING
 function SortContainerItem(search,targetbag)
   numberOfFreeSlots, BagType = GetContainerNumFreeSlots(BagNumberConversion(targetbag))
   if numberOfFreeSlots >= 1 then
@@ -86,9 +113,12 @@ function SortContainerItem(search,targetbag)
           iname, ilink, iRarity, iLevel, ireqLevel, iclass, isubclass, imaxStack, iequipSlot, itexture, ivendorPrice = GetItemInfo(item)
         end
         if item and search == iname then
-          
-          PickupContainerItem(bag,slot)
-          if targetbag == "1" then PutItemInBackpack() else PutItemInBag(targetbag) end
+          if targetbag == 1 then 
+            PickupContainerItem(bag,slot)
+            PutItemInBackpack() else
+            PickupContainerItem(bag,slot)
+            PutItemInBag(targetbag)
+          end
         end
       end
     end
@@ -96,118 +126,6 @@ function SortContainerItem(search,targetbag)
   end
   ResetCursor()
 end
-
--- Move all items into "targetbag" based on item rarity
-function SortItemsRarity(targetbag,targetrarity)
-  for bag = 0,4 do
-    for slot = 1,GetContainerNumSlots(bag) do
-      local item = GetContainerItemLink(bag,slot)
-      if item then 
-       iname, ilink, iRarity, iLevel, ireqLevel, iclass, isubclass, imaxStack, iequipSlot, itexture, ivendorPrice = GetItemInfo(item)
-  	  end
-      if item and iRarity == targetrarity then
-       PickupContainerItem(bag,slot)
-       if targetbag == "1" then PutItemInBackpack() else PutItemInBag(targetbag) end
-      end
-    end
-  end
-  ResetCursor()
-end
-
-
--- Sort all items into "targetbag" based on what type of tradeskill bag they fit into 
-function SortItemsType(targetbag,targetfamily)
-  for bag = 0,4 do
-    for slot = 1,GetContainerNumSlots(bag) do
-      local item = GetContainerItemLink(bag,slot)
-      local itemfamily = GetItemFamily(item)
-      if item and itemfamily == targetfamily then
-        PickupContainerItem(bag,slot)
-        if targetbag == "1" then PutItemInBackpack() else PutItemInBag(targetbag) end
-      end
-    end
-  end
-  ResetCursor()
-end
-
-function SellGrey()
-  x = 0
-  totalsale = 0
-  for bag = 0,4 do
-    for slot = 1,GetContainerNumSlots(bag) do
-      local item = GetContainerItemLink(bag,slot)
-      if item then 
-       iname, ilink, iRarity, iLevel, ireqLevel, iclass, isubclass, imaxStack, iequipSlot, itexture, ivendorPrice = GetItemInfo(item)
-      end
-      if item and iRarity == 0 and ivendorPrice > 0 then
-       PickupContainerItem(bag,slot)
-       PickupMerchantItem(0)
-       itemcount = tonumber(GetItemCount(ilink))
-       totalsale = totalsale + (itemcount * tonumber(ivendorPrice))
-       x = x + itemcount
-      end
-    end
-  end
-  if x > 0 and SmartBagSettings["Alerts"] == true then
-    print("<SmartBag> Total Grey Items Sold: " .. x )
-    print("<SmartBag> Total Sale Price: " .. ConvertToWoWMoney(totalsale) )
-  end
-  ResetCursor()
-end
-
-function ConvertToWoWMoney(number)
-
-  if number > 9999 then
-    gold = removeDecimal((number / 10000))
-    goldtemp = gold * 10000
-    silver = removeDecimal((number - goldtemp) / 100)
-    silvertemp = silver * 100
-    copper = number - goldtemp - silvertemp
-    wowmoney = gold .. " gold " .. silver .. " silver " .. copper .. " copper"
-  elseif number > 99 then
-    silver = removeDecimal(number / 100)
-    silvertemp = silver * 100
-    copper = number - silvertemp
-    wowmoney = silver .. " silver " .. copper .. " copper"
-  elseif number then
-    wowmoney = number .. " copper"
-  end
-
-  return wowmoney
-
-end
- 
-function removeDecimal(number)
-  local num = number
-  local num2 = tostring(num)
-  local found = nil
-  for i=1,100000 do
-    if string.sub(num2,i,i) == "." then
-      found = i
-    end
-  end
-  if type(found) == "number" then
-    num2 = string.sub(num2,1,found-1)
-  end
-  num = tonumber(num2)
-  return num
-end
-
-
-function WhatBag(search)
-    for bag = 0,4 do
-      for slot = 1,GetContainerNumSlots(bag) do
-        local item = GetContainerItemLink(bag,slot)
-        if item and item:find(search) then
-        foundbag = bag
-        foundslot = slot
-        end
-      end
-    end
-  return foundbag
-end
-
-
 
 -- Sort Equipement from sets into "targetbag"
 function SortEquipmentSet(targetbag)
@@ -255,41 +173,101 @@ function SortEquipmentSet(targetbag)
   ResetCursor()
 end
 
--- Tests the item and returns true if it is a member of any Gear Set in the equipment manager
-function IsSetItem(itemname)
-  itemsetstatus = false
+function SortRarity(targetrarity,targetbag)
+  --targetrarity 0-10 , targetbag 1(backpack),20-23(bags),IncSet 0/1
+  for bag = 0,4 do
+    for slot = 1,GetContainerNumSlots(bag) do
+      local item = GetContainerItemLink(bag,slot)
+      if item then
+        iname, ilink, iRarity, iLevel, ireqLevel, iclass, isubclass, imaxStack, iequipSlot, itexture, ivendorPrice = GetItemInfo(item)
+        if iRarity == targetrarity and IsSetItem(iname) == false then
+          if targetbag == 1 then 
+            PickupContainerItem(bag,slot)
+            PutItemInBackpack() else
+            PickupContainerItem(bag,slot)
+            PutItemInBag(targetbag)
+          end
+        end
+      end
+    end
+  end
+  ResetCursor()
+end
+
+-- Sort Equipement from sets into "targetbag"
+function SortEquipmentSet(targetbag)
+  x = 0
   for equipset = 1,GetNumEquipmentSets() do 
     name, icon, lessIndex = GetEquipmentSetInfo(equipset)
     itemArray = GetEquipmentSetItemIDs(name);
     for itemslot = 1,19 do 
       if GetItemInfo(itemArray[itemslot]) then
         iname, ilink, iRarity, iLevel, ireqLevel, iclass, isubclass, imaxStack, iequipSlot, itexture, ivendorPrice = GetItemInfo(itemArray[itemslot])
-        if itemname == iname then itemsetstatus =true
-        end
+        superbag = tonumber(WhatBag(iname))
+        supertarget = tonumber(BagNumberConversion(targetbag))
+        if IsEquippedItem(ilink) == 1 or superbag == supertarget then
+          --Goodtimes 
+        else
+          x = x +1
+        end        
       end
     end
   end
-  return itemsetstatus
+  numberOfFreeSlots, BagType = GetContainerNumFreeSlots(BagNumberConversion(targetbag))
+  if numberOfFreeSlots >= x then
+  for equipset = 1,GetNumEquipmentSets() do 
+    name, icon, lessIndex = GetEquipmentSetInfo(equipset)
+    itemArray = GetEquipmentSetItemIDs(name);
+    for itemslot = 1,19 do 
+      if GetItemInfo(itemArray[itemslot]) then
+        iname, ilink, iRarity, iLevel, ireqLevel, iclass, isubclass, imaxStack, iequipSlot, itexture, ivendorPrice = GetItemInfo(itemArray[itemslot])
+        if IsEquippedItem(ilink) == 1 or WhatBag(iname) == BagNumberConversion(targetbag) then
+          --Goodtimes
+        else
+          SortContainerItem(iname,targetbag)
+        end        
+      end
+    end
+  end
+  else
+    if SmartBagSettings["Alerts"] == true then
+      print("<SmartBag> Equipment Sorting: Not enough free space.")
+      print("<SmartBag> Equipment Sorting: Required space: " .. x )
+      print("<SmartBag> Equipment Sorting: Available space: " .. numberOfFreeSlots)
+      print("<SmartBag> Equipment Sorting: Target: " .. KeepEquipmentButton:GetText() )
+    end
+  end
+  ResetCursor()
 end
 
--- Yeah... this doesn't work yet.....
-function SortNonEquipmentSetItems(targetbag,targetrarity)
+function SellGrey()
+  x = 0
+  totalsale = 0
   for bag = 0,4 do
     for slot = 1,GetContainerNumSlots(bag) do
       local item = GetContainerItemLink(bag,slot)
       if item then 
        iname, ilink, iRarity, iLevel, ireqLevel, iclass, isubclass, imaxStack, iequipSlot, itexture, ivendorPrice = GetItemInfo(item)
       end
-      if item and iRarity == targetrarity and IsSetItem(iname) == false then
-        SortContainerItem(iname,targetbag)
+      if item and iRarity == 0 and ivendorPrice > 0 then
+       PickupContainerItem(bag,slot)
+       PickupMerchantItem(0)
+       itemcount = tonumber(GetItemCount(ilink))
+       totalsale = totalsale + (itemcount * tonumber(ivendorPrice))
+       x = x + itemcount
       end
     end
   end
+  if x > 0 and SmartBagSettings["Alerts"] == true then
+    print("<SmartBag> Total Grey Items Sold: " .. x )
+    print("<SmartBag> Total Sale Price: " .. ConvertToWoWMoney(totalsale) )
+  end
+  ResetCursor()
 end
 
-function sbtest()
-  if IsSetItem("Inscribed Crane Staff") == false then print("FAIL") elseif  IsSetItem("Valley Stir Fry") == true then print("YES") end
-end
+-- *********************************************
+-- Utility Functions
+-- *********************************************
 
 function BagNumberConversion(value)
   if value == "1" then value = "0" end 
@@ -324,25 +302,69 @@ function UpdateSettingChoice(value)
    return value
 end
 
--- *********************************************
--- UI Functions
--- *********************************************
-
-
-function SellGreyButton_OnClick()
-   SmartBagSettings["AutoSellGrey"] = UpdateSettingChoice(SmartBagSettings["AutoSellGrey"])
-   SetButttonText(SellGreyButton,SmartBagSettings["AutoSellGrey"])
-end
-
-function KeepEquipmentButton_OnClick()
-   SmartBagSettings["GearSetBag"] = UpdateSettingChoice(SmartBagSettings["GearSetBag"])
-   SetButttonText(KeepEquipmentButton,SmartBagSettings["GearSetBag"])
+function ConvertToWoWMoney(number)
+  if number > 9999 then
+    gold = removeDecimal((number / 10000))
+    goldtemp = gold * 10000
+    silver = removeDecimal((number - goldtemp) / 100)
+    silvertemp = silver * 100
+    copper = number - goldtemp - silvertemp
+    wowmoney = gold .. " gold " .. silver .. " silver " .. copper .. " copper"
+  elseif number > 99 then
+    silver = removeDecimal(number / 100)
+    silvertemp = silver * 100
+    copper = number - silvertemp
+    wowmoney = silver .. " silver " .. copper .. " copper"
+  elseif number then
+    wowmoney = number .. " copper"
   end
-function AlertTextButton_OnClick()
-   SmartBagSettings["Alerts"] = UpdateSettingChoice(SmartBagSettings["Alerts"])
-   SetButttonText(AlertTextButton,SmartBagSettings["Alerts"])
+
+  return wowmoney
+
+end
+ 
+function removeDecimal(number)
+  local num = number
+  local num2 = tostring(num)
+  local found = nil
+  for i=1,100000 do
+    if string.sub(num2,i,i) == "." then
+      found = i
+    end
+  end
+  if type(found) == "number" then
+    num2 = string.sub(num2,1,found-1)
+  end
+  num = tonumber(num2)
+  return num
 end
 
-function OkButton_OnClick()
-  SmartBagSettingsWindow:Hide()
+function WhatBag(search)
+    for bag = 0,4 do
+      for slot = 1,GetContainerNumSlots(bag) do
+        local item = GetContainerItemLink(bag,slot)
+        if item and item:find(search) then
+        foundbag = bag
+        foundslot = slot
+        end
+      end
+    end
+  return foundbag
+end
+
+-- Tests the item and returns true if it is a member of any Gear Set in the equipment manager
+function IsSetItem(itemname)
+  itemsetstatus = false
+  for equipset = 1,GetNumEquipmentSets() do 
+    name, icon, lessIndex = GetEquipmentSetInfo(equipset)
+    itemArray = GetEquipmentSetItemIDs(name);
+    for itemslot = 1,19 do 
+      if GetItemInfo(itemArray[itemslot]) then
+        iname, ilink, iRarity, iLevel, ireqLevel, iclass, isubclass, imaxStack, iequipSlot, itexture, ivendorPrice = GetItemInfo(itemArray[itemslot])
+        if itemname == iname then itemsetstatus =true
+        end
+      end
+    end
+  end
+  return itemsetstatus
 end
