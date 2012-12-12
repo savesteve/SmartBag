c252b88f3c7b4c355cfbe2dfff2c10d31e629bf1
Steve Tibbetts <steve@scrubbingmachine.com>
2012-12-12 10:49:16 -0800

    Changed version numbers.

--- SmartBag.lua
+++ SmartBag.lua
@@ -2,7 +2,7 @@
 -- Addon Setup
 -- *********************************************
 function SmartBag_OnLoad()
-	print("|cFF0066FF<SmartBag |cFFFFFF00v1.01|cFF0066FF>")
+	print("|cFF0066FF<SmartBag |cFFFFFF00v2.0|cFF0066FF>")
 	SlashCmdList["SMARTBAG"] = SmartBag_SlashCommand
 	SLASH_SMARTBAG1 = "/smartbag"
 	SLASH_SMARTBAG2 = "/sb"
